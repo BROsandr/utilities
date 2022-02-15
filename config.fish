13a68dc7b1a795f2d05cfdd5b9bbf990b2d20d04
BROsander <53278658+BROsandr@users.noreply.github.com>
2022-02-15 20:52:18 +0300

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Don't shorten the working directory in the prompt
+    set -g fish_prompt_pwd_dir_length 0
+end
